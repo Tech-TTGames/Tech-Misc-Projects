1a4c04058e05e4da92cad464c092850449aa58ab
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2023-01-10 18:04:41 +0100

    Dumed bad HTML filter

--- reg.py
+++ reg.py
@@ -1,30 +1,24 @@
+'''Regex '''
+from os import system
+
 import re
 import pyperclip
-from os import system
 
 if input("Enter 1 to use current clipboard;\n") == '1':
     a = pyperclip.paste()
 else:
     print("Ready! Please copy the text to filter.")
     a = pyperclip.waitForNewPaste()
-if input('Enter 1 for HTML filtering:\n') != '1':
     l = re.findall(r'[0-9]{18}',a)
     chunks = [l[x:x+100] for x in range(0, len(l), 100)]
     if len(chunks) == 1:
-        ls = ' '.join([str(elem) for elem in chunks[0]])
-        pyperclip.copy(ls)
+        result = ' '.join([str(elem) for elem in chunks[0]])
+        pyperclip.copy(result)
         print("Done! The filtered results are in your clipboard!")
         system('pause')
     else:
-        for id in range(len(chunks)):
-            ls = ' '.join([str(elem) for elem in chunks[id]])
-            pyperclip.copy(ls)
-            print(f"Done! The filtered result {id+1} out of {len(chunks)} is in your clipboard!")
+        for o in range(len(chunks)):
+            result = ' '.join([str(elem) for elem in chunks[o]])
+            pyperclip.copy(result)
+            print(f"Done! The filtered result {o+1} out of {len(chunks)} is in your clipboard!")
             system('pause')
-else:
-    l = re.findall(r'(?s)(?<=<h3 class="post-title entry-title" itemprop="name">\r\n )(.*?)(?=</h3>)',a)
-    ls = ' '.join([str(elem) for elem in l])
-    print(ls)
-    pyperclip.copy(ls)
-    print("Done! The filtered results are in your clipboard!")
-    system('pause')
